8ca3df84dbc2b1c1c753e88213546369a0c40b06
shreyash015 <shreyash015@gmail.com>
2026-08-14 14:02:45 -0500

    added currency card

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WeatherCard } from './components/weather-card/weather-card';
 import { HealthRing } from './components/health-ring/health-ring';
+import { CurrencyCard } from './components/currency-card/currency-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WeatherCard, HealthRing],
+  imports: [RouterOutlet, WeatherCard, HealthRing, CurrencyCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/currency-card/currency-card.ts
+++ src/app/components/currency-card/currency-card.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-currency-card',
-  imports: [],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './currency-card.html',
   styleUrl: './currency-card.css',
 })
-export class CurrencyCard {}
+export class CurrencyCard {
+  amount = signal(100);
+  fromCurrency = signal('INR');
+  toCurrency = signal('USD');
+  currency: Record<string, string> = { USD: '$', EUR: '€', GBP: '£', INR: '₹' };
+  rates: Record<string, number> = { USD: 1, EUR: 0.92, GBP: 0.79, INR: 83.2 };
+  converted = computed(() => {
+    const usd = this.amount() / this.rates[this.fromCurrency()];
+    return usd * this.rates[this.toCurrency()];
+  });
+}
